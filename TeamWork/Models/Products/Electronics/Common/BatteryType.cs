65fd2fca9b60814248a271647938d872963a2906
NevenaN <n_milanova@mail.bg>
2018-07-27 09:43:27 +0300

    Added Phone.cs, BatteryType.cs, Colors.cs

---
+++ TeamWork/Models/Products/Electronics/Common/BatteryType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamWork.Models.Products.Electronics.Common
+{
+    public enum BatteryType
+    {
+        LiIon,
+        NiMH,
+        NiCd
+    }
+}
---
+++ TeamWork/Models/Products/Electronics/Common/Colors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamWork.Models.Products.Electronics.Common
+{
+    public enum Colors
+    {
+        Black,
+        White,
+        Red,
+        Grey,
+        Blue,
+        Gold
+    }
+}
