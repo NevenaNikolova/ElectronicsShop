a8c2982a90c8d6d62222a9dd865b51ad0465ca7b
NevenaN <n_milanova@mail.bg>
2018-07-29 11:23:27 +0300

    Changed Smartphone.cs and LandlinePhone.cs.

--- TeamWork/Models/Products/Electronics/Phones/Smartphone.cs
+++ TeamWork/Models/Products/Electronics/Phones/Smartphone.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TeamWork.Models.Electronics;
+using TeamWork.Models.Electronics.AbstractClasses;
+using TeamWork.Models.Products.Electronics.Common;
 
 namespace TeamWork.Models.Electronics.Phones
 {
-    class Smartphone
+    public class Smartphone:Phone
     {
+        private string processor;
+        private int ram;
+
+        public Smartphone(string productName, decimal price) : base(productName, price)
+        {
+        }
+
+        public Smartphone(string productName, decimal price, string brand, string model, Colours colour, BatteryType battery, 
+            int displaySize, double height, double width, double thickness, string processor, int ram) : base(productName, price, brand, model, colour, battery, displaySize, height, width, thickness)
+        {
+            this.Procesor = processor;
+            this.Ram = ram;
+        }
+        public string Procesor { get; set; }
+        public int Ram { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Phone: {0}, price: {1}, brand: {2}, model: {3}, colour: {4}, battery: {5}, " +
+                "display size: {6}, height: {7}, width: {8}, thickness: {9}, processor: {10}, ram: {11}",
+                this.ProductName, this.Price, this.Brand, this.Model, this.Colour, this.Battery, this.DisplaySize, this.Height, this.Width, this.Thickness, this.Procesor, this.Ram);
+        }
     }
 }
