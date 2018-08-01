b664ffc50c38c229d941edde162ca4da720dc095
Shaban Isa <shaban9726@gmail.com>
2018-08-01 11:41:15 +0300

    adding Exception

---
+++ TeamWork/Core/Tools/UserException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectronicsShop.Core.Tools
+{
+    public class UserException : Exception
+    {
+        public UserException() : base()
+        { }
+        public UserException(String message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
