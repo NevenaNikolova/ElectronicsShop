786a822dd2e1f5ffe68f3e4de5fc40076bf46b56
NevenaN <n_milanova@mail.bg>
2018-08-01 11:38:50 +0300

    renamed project

---
+++ TeamWork/Core/Contracts/ILogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElectronicsShop.Core.Contracts
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+}
---
+++ TeamWork/Core/Tools/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ElectronicsShop.Core.Contracts;
+
+namespace ElectronicsShop.Core.Tools
+{
+    public class Logger : ILogger
+    {
+        public Logger()
+        {
+
+        }
+        public void Log(string message)
+        {
+            File.AppendAllText("LogFile.txt", message);
+        }
+    }
+}
