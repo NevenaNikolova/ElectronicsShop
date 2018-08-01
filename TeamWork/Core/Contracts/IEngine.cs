5b686aece28441d6738fe96520b40227adbce71f
Viktor Doninski <vicktord@mail.bg>
2018-08-01 11:15:28 +0300

    Tools folder created

---
+++ TeamWork/Core/Contracts/IEngine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamWork.Core.Contracts
+{
+    interface IEngine
+    {
+        void Start();
+    }
+}
---
+++ TeamWork/Models/Interfaces/IPC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TeamWork.Models;
+using TeamWork.Models.Interfaces;
+
+namespace TeamWork.Models.Interfaces
+{
+    public interface IPC : IProduct
+    {
+
+        string Brand { get; }
+
+        string Model { get; }
+
+        string Processor { get; }
+
+        int Ram { get; }
+
+        int Hdd { get; }
+
+        int VideoCard { get; }
+
+    }
+}
