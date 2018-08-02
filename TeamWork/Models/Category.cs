796ee57ba9dfbc72eaad6ead97bcacb8b93c37f7
NevenaN <n_milanova@mail.bg>
2018-08-02 13:23:55 +0300

    changed Category.cs

--- TeamWork/Models/Category.cs
+++ TeamWork/Models/Category.cs
@@ -1,42 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
 using ElectronicsShop.Models.Electronics.AbstractClasses;
+using ElectronicsShop.Models.Electronics.Phones;
 using ElectronicsShop.Models.Interfaces;
 
 namespace ElectronicsShop.Models
 {
     public class Category
     {
-        private readonly string name;
-        private List<IProduct> products;
+        private string name;
         private List<Laptop> laptops;
+        private List<DesktopPC> desktopPCs;
+        private List<Smartphone> smartphones;
+        private List<LandlinePhone> landlinePhones;
 
         public Category(string name)
         {
-            this.name = name;
-            this.products = new List<IProduct>();
+            this.Name = name;
             this.laptops = new List<Laptop>();
+            this.desktopPCs = new List<DesktopPC>();
+            this.smartphones = new List<Smartphone>();
+            this.landlinePhones = new List<LandlinePhone>();
         }
 
         public string Name
         {
             get { return this.name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException();
+                }
+                this.name = value;
+            }
         }
+
+        public List<Laptop> Laptops { get => new List<Laptop>(this.laptops); }
+        public List<DesktopPC> DesktopPCs { get => new List<DesktopPC>(this.desktopPCs); }
+        public List<Smartphone> Smartphones { get => new List<Smartphone>(this.smartphones); }
+        public List<LandlinePhone> LandlinePhones { get => new List<LandlinePhone>(this.landlinePhones); }
 
         public void addProduct(IProduct product)
         {
-            if (product.GetType().Name == "Laptop")
+            switch (product.GetType().Name)
             {
-               this.laptops.Add((Laptop)product);
-            }
-            
-        }
+                case "Laptop":
+                    this.Laptops.Add((Laptop)product);
+                    break;
+                case "Desktop Computer":
+                    this.DesktopPCs.Add((DesktopPC)product);
+                    break;
+                case "Smartphone":
+                    this.Smartphones.Add((Smartphone)product);
+                    break;
+                case "LandlinePhone":
+                    this.LandlinePhones.Add((LandlinePhone)product);
+                    break;
+                default:
+                    throw new ArgumentException("There is no such type!");
 
+            }
+        }
         public void RemoveProduct(IProduct product)
         {
-            if (this.products.Contains(product))
+            switch (product.GetType().Name)
             {
-                this.products.Remove(product);
+                case "Laptop":
+                    {
+                        if (laptops.Contains((Laptop)product))
+                        {
+                            this.Laptops.Remove((Laptop)product);
+                        }
+                    }
+                    break;
+                case "Desktop Computer":
+                    {
+                        if (desktopPCs.Contains((DesktopPC)product))
+                        {
+                            this.DesktopPCs.Remove((DesktopPC)product);
+                        }
+                    }
+                    break;
+                case "Smartphone":
+                    {
+                        if (smartphones.Contains((Smartphone)product))
+                        {
+                            this.Smartphones.Remove((Smartphone)product);
+                        }
+                    }
+                    break;
+                case "LandlinePhone":
+                    {
+                        if (landlinePhones.Contains((LandlinePhone)product))
+                        {
+                            this.LandlinePhones.Remove((LandlinePhone)product);
+                        }
+                    }
+                    break;
+                default:
+                    throw new ArgumentException("Product not found!");
+
             }
         }
 
